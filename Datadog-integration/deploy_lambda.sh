fdd0a89b2cab81107e43f4c4539efc7bc6b62be1
Rahul K <117668803+rahuleficens@users.noreply.github.com>
2024-02-20 14:22:21 +0530

    .

--- Datadog-integration/deploy_lambda.sh
+++ Datadog-integration/deploy_lambda.sh
@@ -1,6 +1,4 @@
-            # Bash script in deploy_lambda.sh
-aws lambda create-function --function-name MyLambdaFunction \
-    --runtime python3.8 \
-    --role arn:aws:iam::123456789012:role/lambda-role \
-    --handler lambda_function.lambda_handler \
-    --zip-file fileb://lambda_code.zip
+aws cloudformation create-stack \
+  --stack-name YourStackName \
+  --template-body file://path/to/your-template.yaml \
+  --region YourAWSRegion
